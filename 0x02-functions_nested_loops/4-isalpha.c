93440a3cbf5f741fc9c26f895498db3277d0aa18
Nadeen Allam <nadeenallam2023@gmail.com>
2024-07-07 06:49:03 -0700

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,10 +8,12 @@
  *
  * Return: 1 is c if true else 0
 */
+
 int _isalpha(int c)
 {
 	if (c >= 97 && c <= 122)
 		return (1);
 	if (c >= 65 && c <= 90)
-		return (0);
+		return (1);
+	return (0);
 }
