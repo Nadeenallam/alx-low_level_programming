36e2d05de0f46b56315e17334f3c50d9b2608ad6
Nadeen Allam <nadeenallam2023@gmail.com>
2024-07-07 08:54:28 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,8 +4,8 @@
  * times_table - prints the 9 time table
  *
  * Example Table
- * 0, 0, 0, 0, ..
- * 0, 1, 2, 3, ..
+ * 0,  0,  0,  0, ..
+ * 0,  1,  2,  3, ..
  *
 */
 
@@ -15,24 +15,24 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		putchar(48);
+		_putchar(48);
 		for (mult = 1; mult <= 9; mult++)
 		{
-			_putchar(',');
+		_putchar(',');
+		_putchar(' ');
+
+		prod = num * mult;
+
+		/*
+		 * put space if product is a single number
+		 * place the first digit if its two numbers
+		*/
+		if (prod <= 9)
 			_putchar(' ');
-
-			prod = num * mult;
-
-			/*
-			 * put space if product is a single number
-			 * place the first digit if its two numbers
-			*/
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + 48); /*get the firdt digit*/
-			_putchar((prod % 10) + 48); /*get the second digit*/
-		}
-		_putchar('\n');
+		else
+			_putchar((prod / 10) + 48); /*get the firdt digit*/
+		_putchar((prod % 10) + 48); /*get the second digit*/
+	}
+	_putchar('\n');
 	}
 }
