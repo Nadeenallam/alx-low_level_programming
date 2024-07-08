120ef3aac6268b6d43eb7088b284370eeca5740f
Nadeen Allam <nadeenallam2023@gmail.com>
2024-07-08 02:56:28 -0700

    16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -43,7 +43,7 @@ int main(void)
 			printf("%lu", f1o);
 		initia10s = numLength(mx) - 1 - numLength(f1);
 
-		while (f1o > 0 && intia10s > 0)
+		while (f1o > 0 && initia10s > 0)
 		{
 			printf("%d", 0);
 			initia10s--;
