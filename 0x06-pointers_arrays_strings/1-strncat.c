4676e096ae02e07931f99f8a5e563a3aae2386b1
Nadeen Allam <nadeenallam2023@gmail.com>
2024-07-08 11:53:14 -0700

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *  *_strncat - a function that concatenates two strings
- *
+ *  _strncat - a function that concatenates two strings
+ *f
  * @dest: pointer to destination input
  * @src: pointer to source input
  * @n: most number of bytes from @src
@@ -27,7 +27,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[c + i] = src[i];
 	/*null terminate dest*/
-	dest[c + i] = '0';
+	dest[c + i] = '\0';
 
 	return (dest);
 }
