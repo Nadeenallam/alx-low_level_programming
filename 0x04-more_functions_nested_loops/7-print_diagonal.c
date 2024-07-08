e435cb0862a6a38f4eb61cb483d07b68b6683be1
Nadeen Allam <nadeenallam2023@gmail.com>
2024-07-08 06:04:42 -0700

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,15 +9,15 @@
 
 void print_diagonal(int n)
 {
-	int postn, space;
+	int linenum, space;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (postn = 1; postn <= n; postn++)
+		for (linenum = 1; linenum <= n; linenum++)
 		{
-			for (space = 1; space <= postn; space++)
+			for (space = 1; space <= linenum; space++)
 				_putchar(' ');
 			_putchar(92); /*is equal to '/' char*/
 			_putchar('\n');
