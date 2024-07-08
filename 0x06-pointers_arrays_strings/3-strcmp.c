6a07898e3907c72d5aa9e02c6f0a6a6ec42c339d
Nadeen Allam <nadeenallam2023@gmail.com>
2024-07-08 12:39:12 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _strcmp - compares two strings
+ * _strcmpi - compares two strings
  * @s1: string 1
  * @s2: string 2
  * Return: 1 if true, 0 if false
 */
 
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
 {
 	int equal = 0;
 
@@ -18,9 +18,8 @@ int _strcmp(char *s1, char *s2);
 			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
 			break;
 		}
-		s1++;
-		s2++;
-
+	s1++;
+	s2++;
 	}
 	return (equal);
 }
