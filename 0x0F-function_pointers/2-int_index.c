ae24cfc19bdfd50b87902c7f251b33840a41579d
Nadeen Allam <nadeenallam2023@gmail.com>
2024-07-24 19:52:54 -0700

    0

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,24 +1,23 @@
 #include "function_pointers.h"
 
 /**
- * int_index - searches for intrger
+ * int_index - searches for integer
  * @array: the int array
  * @size: the array size
- * @cmp: the compare function
+ * @cmp:the compare function
  *
- * Return: the intrger index
+ *Return: the inteher index
 */
-
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array && size && cmp)
+	if (array && size cmp)
 		while (i < size)
 		{
 			if (cmp(array[i]))
 				return (i);
 			i++;
 		}
-	return (-1)
+	return (-1);
 }
