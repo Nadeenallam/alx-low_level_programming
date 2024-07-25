176d4f864b014786a2ca81514661a101c466d135
Nadeen Allam <nadeenallam2023@gmail.com>
2024-07-24 20:49:00 -0700

    0

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the sum
+*/
+int op_add(int a, int b)
+{
+	return(a + b);
+}
+
+/**
+ * op_sub -subtract two integers
+ * @a: te first int
+ * @b: the second int
+ *
+ * Return: the difference
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_div divides two integes
+ * @a: the fisrt int
+ * @b: the secount int
+ *
+ * Return: the quotient
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two integers
+ * @a: the firdt int
+ * @b: the secound int
+ *
+ * Return: the quotient
+*/
+int op_dic(int a, intb)
+{
+	return (a / b);
+}
+
+
